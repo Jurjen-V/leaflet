30c1c45b3b50bbe3546f9617fa0fd7f55ddf4cfe
Jurjen <jurjen.veenstra@hotmail.nl>
2020-02-12 16:49:04 +0100

    zoek balkk

--- mapsroute.php
+++ mapsroute.php
@@ -74,8 +74,7 @@
     </li>
 </ul>
 
-<div class="leaflet-top leaflet-left">
-    <br><br><br>
+<div class="leaflet-top leaflet-left sidenav_box" id="sidenav">
     <a href="#" id ="sidenav" data-target="slide-out" class="leaflet-control sidenav-trigger" style="cursor: pointer;"><i class="material-icons">menu</i></a>
 </div>
 
